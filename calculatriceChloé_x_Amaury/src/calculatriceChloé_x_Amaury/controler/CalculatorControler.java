0d88fe3ea879f67afc9ff2396da9505b8f3df94e
Amaury Fd <amaury.fumard@gmail.com>
2023-10-18 14:03:57 +0200

    update 13/10

---
+++ calculatriceChloé_x_Amaury/src/calculatriceChloé_x_Amaury/controler/CalculatorControler.java
@@ -0,0 +1,18 @@
+package calculatriceChloé_x_Amaury.controler;
+
+import java.util.List;
+
+public class CalculatorControler implements CalculatorControlerInterface {
+
+	@Override
+	public void change(String accu) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void change(List<Double> stackDatea) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ calculatriceChloé_x_Amaury/src/calculatriceChloé_x_Amaury/controler/CalculatorControlerInterface.java
@@ -0,0 +1,9 @@
+package calculatriceChloé_x_Amaury.controler;
+
+import java.util.List;
+
+public interface CalculatorControlerInterface {
+	public void change(String accu);
+	
+	public void change(List<Double> stackDatea);
+}
